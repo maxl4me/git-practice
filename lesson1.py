73fd0859086ffc39bb2ced9b82fde95c6de17aef
rivking <maxlu4me@gmail.com>
2025-08-11 20:52:23 +0300

    Lesson 1: Pytho syntax, variables, and data types

---
+++ lesson1.py
@@ -0,0 +1,20 @@
+# lesson1.py
+# First Python lesson - Syntax, variables, data types
+
+name = "Max"
+age = 40 
+is_qa = True
+skills = ["Git", "Python", "Postman"]
+
+print("Name:", name)
+print("Age:", age)
+print("QA Automation:", is_qa)
+print("Skills:", skills)
+
+# Check data types
+print("Type of name:" , type(name))
+print("Tupe of age:" ,type(age))
+print("Type of is_qa:", type(is_qa))
+print("Type of skills:",type(skills))
+
+
